aaaebc6cc592f0481a2c5b3ced1915ee3f31b414
Oluwafemi <dr.phemmy35@gmail.com>
2021-06-23 21:46:48 +0100

    ecop deploy to heroku

--- ecop_api/settings.py
+++ ecop_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -152,3 +153,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
